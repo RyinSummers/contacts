de3f6b35b2b6d5ac908deec460b10a7c652b8a1d
Ryin <583480035@qq.com>
2016-08-21 13:40:56 +0800

    frag1自动读取手机联系人（然而乱序）

--- app/src/main/java/yaoyaoandus/contacts/FirstFragment.java
+++ app/src/main/java/yaoyaoandus/contacts/FirstFragment.java
@@ -5,15 +5,16 @@ package yaoyaoandus.contacts;
  */
 
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AlphabetIndexer;
 import android.widget.ListView;
 import android.widget.SectionIndexer;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,7 +29,7 @@ public  class FirstFragment extends android.support.v4.app.Fragment {
     ListView listView;
     IndexedSimpleAdapter simpleAdapter;
     List<Map<String,Object>> listItems;
-    AlphabetIndexer alphabetIndexer;
+//    AlphabetIndexer alphabetIndexer;
 
     public FirstFragment() { }
 
@@ -39,61 +40,56 @@ public  class FirstFragment extends android.support.v4.app.Fragment {
         View rootView = inflater.inflate(R.layout.fragment_1, container, false);
         listView = (ListView) rootView.findViewById(R.id.listview_frag1);
 
+
         listItems = new ArrayList<Map<String, Object>>();
         Map<String, Object> temp1 = new HashMap<String, Object>();
-        temp1.put("header",R.drawable.usermain);
-        temp1.put("name","刘婧颖");
-        temp1.put("number", "17865198377");
-        listItems.add(temp1);
-        temp1 = new HashMap<String, Object>();
-        temp1.put("header",R.drawable.usermain);
-        temp1.put("name","刘A");
-        temp1.put("number", "178");
-        listItems.add(temp1);
-        temp1 = new HashMap<String, Object>();
-        temp1.put("header",R.drawable.usermain);
-        temp1.put("name","刘B");
-        temp1.put("number", "17865");
-        listItems.add(temp1);
-        temp1 = new HashMap<String, Object>();
-        temp1.put("header",R.drawable.usermain);
-        temp1.put("name","刘婷婷");
-        temp1.put("number","13822123342");
-        listItems.add(temp1);
-        temp1 = new HashMap<String, Object>();
-        temp1.put("header",R.drawable.usermain);
-        temp1.put("name","江大哥");
-        temp1.put("number","888888");
-        listItems.add(temp1);
-        temp1 = new HashMap<String, Object>();
-        temp1.put("header",R.drawable.usermain);
-        temp1.put("name","宁");
-        temp1.put("number","19191919");
-        listItems.add(temp1);
+//        temp1.put("header",R.drawable.usermain);
+//        temp1.put("name","刘A");
+//        temp1.put("number", "178");
+//        listItems.add(temp1);
+//        temp1 = new HashMap<String, Object>();
+//        temp1.put("header",R.drawable.usermain);
+//        temp1.put("name","刘B");
+//        temp1.put("number", "17865");
+//        listItems.add(temp1);
+//        temp1 = new HashMap<String, Object>();
+//        temp1.put("header",R.drawable.usermain);
+//        temp1.put("name","刘婷婷");
+//        temp1.put("number","13822123342");
+//        listItems.add(temp1);
+//        temp1 = new HashMap<String, Object>();
+//        temp1.put("header",R.drawable.usermain);
+//        temp1.put("name","江大哥");
+//        temp1.put("number","888888");
+//        listItems.add(temp1);
+//        temp1 = new HashMap<String, Object>();
+//        temp1.put("header",R.drawable.usermain);
+//        temp1.put("name","宁");
+//        temp1.put("number","19191919");
+//        listItems.add(temp1);
 
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        //定义一个cursor游标，逐行查找数据库，数据库里有一个name与首字母的对应关系
+        Cursor cursor = getActivity().getContentResolver().query(uri,
+                null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                temp1.put("header",R.drawable.usermain);
+                temp1.put("name",name);
+                temp1.put("number", number);
+                listItems.add(temp1);
+                temp1 = new HashMap<String, Object>();
+            } while (cursor.moveToNext());
+        }
+        //alphabetIndexer = new AlphabetIndexer(cursor, 1, " ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+       // simpleAdapter.setSectionIndexer(alphabetIndexer);
         simpleAdapter=new IndexedSimpleAdapter(getActivity(),listItems,R.layout.frag1_list_item,
                 new String[]{"header","name","number"},
                 new int[]{R.id.image_icon_frag1,R.id.name_frag1,R.id.number_frag1});
-
-
-        //还不清楚cursor是什么
-//        Cursor cursor = getContentResolver().query(uri,
-//                new String[] { "display_name", "sort_key" }, null, null, "sort_key");
-//        if (cursor.moveToFirst()) {
-//            do {
-//                String name = cursor.getString(0);
-//                String sortKey = getSortKey(cursor.getString(1));
-//                Contact contact = new Contact();
-//                contact.setName(name);
-//                contact.setSortKey(sortKey);
-//                contacts.add(contact);
-//            } while (cursor.moveToNext());
-//        }
-//        startManagingCursor(cursor);
-//        alphabetIndexer = new AlphabetIndexer(cursor, 1, "#ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-        //simpleAdapter.setSectionIndexer(alphabetIndexer);
         listView.setAdapter(simpleAdapter);
+        cursor.close();
         return rootView;
     }
 
@@ -133,8 +129,8 @@ public  class FirstFragment extends android.support.v4.app.Fragment {
             } else {
                 v = convertView;
             }
-            //int section = mSectionIndexer.getSectionForPosition(position);
-            TextView sortKey = (TextView) v.findViewById(R.id.alphabet_title_frag1);
+//            int section = mSectionIndexer.getSectionForPosition(position);
+//            TextView sortKey = (TextView) v.findViewById(R.id.alphabet_title_frag1);
 //            if (position == mSectionIndexer.getPositionForSection(section)) {
 //                sortKey.setText("hey");
 //                sortKey.setVisibility(View.VISIBLE);
