1613db3015014e1d644c281b2d2770584d1ddf7c
Ryin <583480035@qq.com>
2016-09-19 19:16:21 +0800

    ryinwork不报错

--- app/src/main/java/yaoyaoandus/contacts/CardInfoActivity.java
+++ app/src/main/java/yaoyaoandus/contacts/CardInfoActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.AlertDialog;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -65,7 +66,7 @@ public class CardInfoActivity extends Activity {
                 }
         );
 
-        Button button_call=(Button)findViewById(R.id.button_makecall);
+        FloatingActionButton button_call=(FloatingActionButton)findViewById(R.id.call);
         button_call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
